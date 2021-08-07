53d47868af915d12562a5e73fecf424b872c3c7e
Kevin Chen <kevin.szu.chen@gmail.com>
2021-08-07 00:34:09 -0500

    Updates

---
+++ precache-manifest.247ee6ce5bb6a2cafe5beae28a8e2d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38aaf70bf5a0d9072e03",
+    "url": "/me/static/css/main.b04d275e.chunk.css"
+  },
+  {
+    "revision": "38aaf70bf5a0d9072e03",
+    "url": "/me/static/js/main.38aaf70b.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "576f39e19218b398266524b4161318b3",
+    "url": "/me/index.html"
+  }
+];
